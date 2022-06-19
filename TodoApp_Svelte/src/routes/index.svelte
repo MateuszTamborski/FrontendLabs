d10e1cd6df42cb49db7f776c06da190417967090
MateuszTamborski <50679915+MateuszTamborski@users.noreply.github.com>
2022-06-19 16:35:48 +0200

    Svelte Todo App

---
+++ TodoApp_Svelte/src/routes/index.svelte
@@ -0,0 +1,208 @@
+<script>
+  let todos = []
+  let task = "";
+	let filter='all';
+
+	function addTask(){
+		todos = [{
+			task:task,
+			status:"incomplete"
+		}, ...todos];
+		task = "";
+	}
+
+	function markComplete(i){
+		if (todos[i].status === "incomplete") {
+			todos[i].status = "completed";
+		} else {
+			todos[i].status = "incomplete"
+		}
+		todos = [...todos];
+	}
+
+	function removeTask(i){
+		todos.splice(i,1);
+		todos = [...todos];
+	}
+
+</script>
+  
+<style>
+	:global(body) {
+		 margin: 0; 
+		 padding: 0; 
+		 box-sizing: border-box;
+	}
+
+	.container {
+		height: 100vh;
+		width: 100vw;
+		background-color: rgb(192, 192, 192);
+		font-family: 'Fira Code', monospace;
+	}
+
+	header {
+		text-align: center;
+		padding: 30px 20px;
+		font-size: xx-large;
+	}
+
+	.todo {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	button {
+		font-family: 'Fira Code', monospace;
+	}
+
+	.filters {
+		width: 335px;
+		display: flex;
+		flex-direction: row;
+		justify-content: space-evenly;
+	}
+
+	.filters button {
+		border: 0;
+		border-radius: 5px;
+		padding: 10px;
+		background-color: #3dc2ff;
+		box-shadow: 0 0 15px 4px rgba(0,0,0,0.06);
+		font-weight: bold;
+	}
+
+	.filters button:hover {
+		background-color: #297ea8;
+	}
+
+	.form {
+		margin: 20px 5px;
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		width: 335px;
+	}
+
+	.form input {
+  	padding: 10px;
+		border: 0;
+		box-shadow: 0 0 15px 4px rgba(0,0,0,0.06);
+		border-radius: 5px;
+		font-family: 'Fira Code', monospace;
+		width: 250px;
+	}
+
+	.form button {
+		border: 0;
+		border-radius: 5px;
+		padding: 10px;
+		background-color: #3dc2ff;
+		box-shadow: 0 0 15px 4px rgba(0,0,0,0.06);
+		font-weight: bold;
+	}
+
+	.form button:hover {
+		background-color: #297ea8;
+	}
+
+	.tasks > .task {
+		margin: 10px 0px;
+		padding: 5px;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		box-shadow: 0 0 15px 4px rgba(0,0,0,0.06);
+		width: 325px;
+		border: 0;
+		border-radius: 5px;
+		background-color: #fff;
+	}
+
+	.tasks > .task > .desc {
+		max-width: 300px;
+	}
+
+	.tasks > .task > .buttons {
+		display: flex;
+		flex-direction: row;
+	}
+
+	.tasks > .task > div > button {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width:25px;
+		height:25px;
+		border: 0;
+		background-color:#ffffff;
+		margin:0px 5px;
+		cursor:pointer;
+	}
+
+	.tasks > .task > div > button.active {
+		border-radius: 50%;
+		background:#16da16;
+	}
+
+</style>
+
+<div class="container">
+	<header>TODO'er</header>
+	<div class="todo">
+
+		<div class="filters">
+			<button class="{filter == 'all' ? 'active' : ''}" on:click={() => {filter = 'all'}}>All</button>
+			<button class="{filter == 'completed' ? 'active' : ''}" on:click={() => {filter = 'completed'}}>Completed</button>
+			<button class="{filter == 'incomplete' ? 'active' : ''}" on:click={() => {filter = 'incomplete'}}>Incomplete</button>
+		</div>
+
+		<div class="form">
+			<input type="text" placeholder="To do" bind:value = {task}/>
+			<button on:click={addTask} disabled={task.length === 0}>Add</button>
+		</div>
+
+		<div class="tasks">
+			{#each todos as todo, i}
+				{#if filter=='all'}
+					<div class="task">
+						<div class="desc">{todo.task}</div>
+						<div class="buttons">
+							<button class="{todo.status == 'completed' ? 'active' : ''}" on:click={() => {markComplete(i)}}>
+								<img src="/check-circle.svg" alt="ok"/>
+							</button>
+							<button on:click={() => { removeTask(i)}}>
+								<img src="/trash3-fill.svg" alt="x"/>
+							</button>
+						</div>
+					</div>
+				{:else if filter=='completed'}
+					{#if todo.status=='completed'}
+						<div class="task">
+							<div class="desc">{todo.task}</div>
+							<div class="buttons">
+								<button on:click={() => {removeTask(i)}}>
+									<img src="/trash3-fill.svg" alt="x"/>
+								</button>
+							</div>
+						</div>
+					{/if}
+				{:else}
+					{#if todo.status=='incomplete'}
+						<div class="task">
+							<div class="desc">{todo.task}</div>
+							<div class="buttons">
+								<button class="{todo.status=='completed'?'active':''}" on:click={() => {markComplete(i)}}>
+									<img src="/check-circle-fill.svg" alt="ok"/>
+								</button>
+							</div>
+						</div>
+					{/if}
+				{/if}
+			{/each}
+		</div>
+
+	</div>
+</div>
